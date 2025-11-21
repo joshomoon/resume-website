f7f722d2db511ae39e6f4a2f983054b8d301c345
joshomoon <jom5971@psu.edu>
2025-11-21 05:20:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,21 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    const downloadBtn = document.getElementById("download-pdf");
+
+    downloadBtn.addEventListener("click", () => {
+        const element = document.body; 
+
+        const opt = {
+            margin:       0.5,   
+            filename:     'My_Resume.pdf',
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 2 }, 
+            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+        html2pdf().set(opt).from(element).save();
+    });
+
     const jobs = document.querySelectorAll(".job");
 
     function showToast(message) {
