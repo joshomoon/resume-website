0127717305c423c9cf86b5c6d928341df85c3204
joshomoon <jom5971@psu.edu>
2025-11-21 06:17:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-
     const toggleBtn = document.getElementById("theme-toggle");
+
     if (localStorage.getItem("theme") === "dark") {
         document.body.classList.add("dark-mode");
         toggleBtn.textContent = "Light Mode";
@@ -38,8 +38,8 @@ document.addEventListener("DOMContentLoaded", () => {
             showToast(`Learn more about my role at ${companyName}`);
         });
     });
-
 });
+
 
 
 
