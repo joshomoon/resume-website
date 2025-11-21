92c1d603ba1d7da9bc2834cbac4a2657abf0256e
joshomoon <jom5971@psu.edu>
2025-11-21 04:33:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const toggleBtn = document.getElementById("theme-toggle");
 
 if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark-mode");
-    toggleBtn.textContent = "☀️ Light Mode";
+    toggleBtn.textContent = "Light Mode";
 }
 
 toggleBtn.addEventListener("click", () => {
